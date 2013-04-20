34c4df72402a12ffeb17fdcb936f4d3e593ec781
Rad Jan <rad.jan@mochimedia.com>
2013-04-20 12:17:15 +0800

    move session key to common, add auth decorator

--- common/share.py
+++ common/share.py
@@ -4,6 +4,9 @@ PARTY_STORE_KIND = 'PARTY'
 PARTY_STORE_ROOT = 'ROOT'
 BLOG_STORE_ROOT = 'BLOG'
 
+WEBAPP2_SESSION_KEY = 'my-little-secret_change-this-before-deploy'
+
 from google.appengine.ext import db
 def party_root_key():
     return db.Key.from_path(PARTY_STORE_KIND, PARTY_STORE_ROOT)
+
--- view/base.py
+++ view/base.py
@@ -1,11 +1,13 @@
 import webapp2
 from webapp2_extras import sessions
 
+from common import share
+
 #This is needed to configure the session secret key
 #Runs first in the whole application
 myconfig = {}
 myconfig['webapp2_extras.sessions'] = {
-    'secret_key': 'my-little-secret',
+    'secret_key': share.WEBAPP2_SESSION_KEY,
 }
 
 class BaseSessionHandler(webapp2.RequestHandler):
@@ -24,3 +26,45 @@ class BaseSessionHandler(webapp2.RequestHandler):
     def session(self):
         # Returns a session using the default cookie key.
         return self.session_store.get_session()
+
+    def render_template(self, template_path, params=None):
+        if params is None:
+            params = {}
+        template = share.jinja_env.get_template(template_path)
+        self.response.out.write(template.render(params))
+
+def login_required(handler):
+    """
+         Decorator for checking if there's a user associated with the current session.
+         Will also fail if there's no session present.
+     """
+
+    def check_login(self, *args, **kwargs):
+        if 'user' not in self.session:
+            # If handler has no login_url specified invoke a 403 error
+            try:
+                self.redirect('/login', abort=True)
+            except (AttributeError, KeyError), e:
+                self.abort(403)
+        else:
+            return handler(self, *args, **kwargs)
+
+    return check_login
+
+def sa_required(handler):
+    """
+         Decorator for checking if there's a user associated with the current session.
+         Will also fail if there's no session present.
+     """
+
+    def check_sa(self, *args, **kwargs):
+        # TODO: check system admin
+        if 'user' not in self.session:
+            # If handler has no login_url specified invoke a 403 error
+            try:
+                self.redirect('/login', abort=True)
+            except (AttributeError, KeyError), e:
+                self.abort(403)
+        else:
+            return handler(self, *args, **kwargs)
+    return check_sa
