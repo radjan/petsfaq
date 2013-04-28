a75b10ab88f2057c7f64e929577c61831a2b605e
Tao-Sheng <taosheng.chen@gmail.com>
2013-04-28 21:05:08 +0800

    add 4 RestFul API, updated readme and test curl script

--- test/curl.test.sh
+++ test/curl.test.sh
@@ -1,7 +1,18 @@
 
 # create new hospital
-curl -v -H "Content-Type: application/json" -X PUT --data '{"name":"TEST_Hospital", "description":"test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:9080/api/v1/hospital
+#curl -v -H "Content-Type: application/json" -X PUT --data '{"name":"TEST_Hospital", "description":"test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:9080/api/v1/hospital
 
-curl -v -H "Content-Type: application/json" -X PUT --data '{"name":"hospital for pets", "description":"test test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:9080/api/v1/hospital
+echo "create a hospital ------------ \n"
+curl -H "Content-Type: application/json" -X PUT --data '{"name":"hospital for pets 2", "description":"test test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:9080/api/v1/hospital
 
-curl -v -X GET http://localhost:9080/api/v1/hospital
+echo "create a person -------------- \n"
+curl -H "Content-Type: application/json" -X PUT --data '{"name":"Yun-Tai 001", "email":"test@test.com", "gender":"F", "phone":"3939889"}' http://localhost:9080/api/v1/person
+
+echo "list all hospital ------------ \n"
+curl  -X GET http://localhost:9080/api/v1/hospital
+echo "list all account ------------ \n"
+curl  -X GET http://localhost:9080/api/v1/account
+echo "list all role ------------ \n"
+curl  -X GET http://localhost:9080/api/v1/role
+echo "list all person ------------ \n"
+curl  -X GET http://localhost:9080/api/v1/person
