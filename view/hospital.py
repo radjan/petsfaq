8a885bc430c412461cd329a1d15fb6eec173b953
Rad Jan <rad.jan@mochimedia.com>
2013-05-01 22:37:31 +0800

    add user

--- view/hospital.py
+++ view/hospital.py
@@ -2,13 +2,19 @@ from view import base
 
 from service.hospital import hospital_service
 
+from common import util
+
 class HospitalList(base.BaseSessionHandler):
     def get(self):
         hospitals = hospital_service.list()
-        self.render_template('hospitals.html', {'hospitals':hospitals})
+        params = {'hospitals':hospitals,
+                  'user': util.get_current_user(self.session)}
+        self.render_template('hospitals.html', params)
 
 class HospitalDetail(base.BaseSessionHandler):
     def get(self):
         h_key = self.request.get('id')
         hospital = hospital_service.get(h_key)
-        self.render_template('hospital_detail.html', {'hospital': hospital})
+        params = {'hospital':hospital,
+                  'user': util.get_current_user(self.session)}
+        self.render_template('hospital_detail.html', params)
