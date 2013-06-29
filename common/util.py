b40f7704cc443451ed98366dc519db2905e29d0d
Rad Jan <rad.jan@mochimedia.com>
2013-06-29 15:57:02 +0800

    data model transformation to common helper

--- common/util.py
+++ common/util.py
@@ -52,3 +52,38 @@ def jsonify_response(response, data):
     json.dump(data, io)
     response.write(io.getvalue())
 
+import collections
+from google.appengine.ext import db
+
+def out_format(data):
+    ret = None
+    if isinstance(data, collections.Iterable):
+        ret = []
+        for d in data:
+            ret.append(out_format(d))
+    else:
+        ret = _to_dict(data)
+    return ret
+
+def _to_dict(domain_obj):
+    tmp = {}
+    property_keys = domain_obj.properties().keys()
+    for key in property_keys:
+        prop = domain_obj.properties()[key]
+        v = prop.get_value_for_datastore(domain_obj)
+        if type(v) is list:
+            l = []
+            for item in v:
+                l.append(_to_str(item))
+            tmp[str(key)] = l
+        else:
+            tmp[str(key)] = _to_str(v)
+    tmp['id'] = domain_obj.get_id()
+    return tmp
+
+def _to_str(obj):
+    if isinstance(obj, db.Key):
+        return _to_dict(db.get(obj))
+    return unicode(obj)
+
+def _update_properties(domain, json_obj
