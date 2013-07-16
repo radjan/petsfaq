b776c548eeed1c9d49b60b42e9b599afe38e7495
samuelololol <samuelololol@gmail.com>
2013-07-16 21:11:52 +0800

    fix obj.kind() list again

--- common/util.py
+++ common/util.py
@@ -90,7 +90,7 @@ def _to_dict(domain_obj, tracedmdl=None):
         for add_prop in add_prop_list:
             fl = [] #foreign key list
             #for x in [v for v in domain_obj.__getattribute__(add_prop) if v.kind() not in tracedmdl]:
-            for x in v in domain_obj.__getattribute__(add_prop):
+            for x in domain_obj.__getattribute__(add_prop):
                 fl.append(_to_dict(x, tracedmdl))
             #add model type
             tmp[add_prop] = fl
