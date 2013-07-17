c9d92109006231f216e584b190596791206fe376
Rad Jan <rad.jan@mochimedia.com>
2013-07-18 01:25:15 +0800

    fix test

--- test/curl.test.sh
+++ test/curl.test.sh
@@ -1,20 +1,20 @@
 
 # create new hospital
-curl -v -H "Content-Type: application/json" -X POST --data '{"name":"TEST_Hospital", "description":"test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:8080/api/v1/hospital
+curl -v -H "Content-Type: application/json" -X POST --data '{"name":"TEST_Hospital", "description":"test", "specialty":"save pets life", "area": "ZhongHe", "zipcode": "235", "county": "New Taipei City", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:8080/api/v1/hospital
 
 echo "create a hospital ------------ \n"
-curl -H "Content-Type: application/json" -X POST --data '{"name":"hospital for pets 2", "description":"test test", "specialty":"save pets life", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:8080/api/v1/hospital
+curl -H "Content-Type: application/json" -X POST --data '{"name":"hospital for pets 2", "description":"test test", "specialty":"save pets life", "area": "ZhongHe", "zipcode": "235", "county": "New Taipei City", "address":"no 125 kr road", "phone":"3939889", "emergency":true}' http://localhost:8080/api/v1/hospital
 
 echo "create a person -------------- \n"
 curl -H "Content-Type: application/json" -X POST --data '{"name":"Yun-Tai 001", "email":"test@test.com", "gender":"F", "phone":"3939889"}' http://localhost:8080/api/v1/person
 
-echo "list all hospital ------------ \n"
+echo "\nlist all hospital ------------ \n"
 curl  -X GET http://localhost:8080/api/v1/hospital
-echo "list all account ------------ \n"
+echo "\nlist all account ------------ \n"
 curl  -X GET http://localhost:8080/api/v1/account
-echo "list all role ------------ \n"
+echo "\nlist all role ------------ \n"
 curl  -X GET http://localhost:8080/api/v1/role
-echo "list all person ------------ \n"
+echo "\nlist all person ------------ \n"
 curl  -X GET http://localhost:8080/api/v1/person
 
 
