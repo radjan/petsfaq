4f4df9eeb2eeb84fb888880eb9c4b09f968505c7
Rad Jan <rad.jan@mochimedia.com>
2013-07-27 17:05:24 +0800

    add EntitySpecialtyRel

--- model/specialty.py
+++ model/specialty.py
@@ -1,8 +1,17 @@
 from google.appengine.ext import db
+from model import hospital, role, base
 
-class Specialty(db.Model):
+class Specialty(base.BaseModel, db.Model):
     species = db.StringProperty(default='')
     category = db.StringProperty(default='')
 
-    def get_id(self):
-        return self.key().id()
+class EntitySpecialtyRel(base.BaseModel, db.Model):
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    hospital = db.ReferenceProperty(hospital.Hospital,
+                    collection_name='specialties')
+    vet = db.ReferenceProperty(role.Vet,
+                    collection_name='specialties')
+    specialty = db.ReferenceProperty(Specialty,
+                    collection_name='relations')
+    note = db.StringProperty(default='')
