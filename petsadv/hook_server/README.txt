2c3605c6d79f7f0466908206285b4181d5871c24
petsquare <petsquare@li672-176.members.linode.com>
2013-11-12 10:48:54 +0800

    add hook server

---
+++ petsadv/hook_server/README.txt
@@ -0,0 +1,4 @@
+
+1. fill the 'key' and 'secret' for authentication
+2. webserver reload command
+
---
+++ petsadv/hook_server/hook.py
@@ -0,0 +1,35 @@
+import logging
+log=logging.getLogger()
+
+from bottle import route, run, template, request
+import os
+from subprocess import Popen, PIPE
+hook_key = 'key'
+hook_secret = 'key'
+
+@route('/api/reload', method='GET')
+def github_hook():
+    key = request.query.get('key',None)
+    secret = request.query.get('secret',None)
+    print key
+    print secret
+    if key != None and secret != None:
+        if key == '':
+            return 'fail1';
+        elif secret == '':
+            return 'fail2';
+        else:
+	    os.chdir('/home/petsquare/petsfaq/')
+	    os.system('git pull') 
+
+	    cmd1 = [ "git", "log"]
+
+	    pipe = Popen(cmd1, stdout=PIPE) 
+	    text = pipe.communicate()[0].splitlines()
+            text1 = text[2]
+            text2 = text[4]
+	    return text1+text2
+    else:
+        return 'fail3'
+
+run(host='0.0.0.0', port=35423)
