c699bc87b8d0b55ad4fef9ccb8652db2691abdef
ra2637 <ra2637@gmail.com>
2013-11-28 12:50:27 +0800

    add petMap page

---
+++ petsadv/web-frontend/app/scripts/controllers/petMap.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('webFrontendApp')
+  .controller('PetmapCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
