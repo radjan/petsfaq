64733972a4974d5c18df92e9bee71690bb861ca0
ra2637 <ra2637@gmail.com>
2013-12-04 16:34:42 +0800

    move FB functions into controller.

--- petsadv/web-frontend/app/scripts/controllers/topNavBar.js
+++ petsadv/web-frontend/app/scripts/controllers/topNavBar.js
@@ -5,47 +5,50 @@ angular.module('webFrontendApp')
     $scope.navBarList = [
       {name:"寵物地圖", url:"/#petMap"}
     ];
-    
+  
+    $scope.initFb = function(){
+      window.fbAsyncInit = function() {
+        /*global FB */
+        FB.init({
+          appId      : '447182208718938', // App ID
+          channelUrl : '7fd7975b33e7c21185d476ae1141314c', // Channel File
+          status     : true, // check login status
+          cookie     : true, // enable cookies to allow the server to access the session
+          xfbml      : true  // parse XFBML
+        });
+      };
+
+      (function(d){
+         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement('script'); js.id = id; js.async = true;
+         js.src = "//connect.facebook.net/en_US/all.js";
+         ref.parentNode.insertBefore(js, ref);
+       }(document));
+    };
+
     $scope.loginByFB=function(){
-    /*global FB */
-		FB.login(function(response) {
-			console.log(response.status);
-		    if (response.authResponse) {
-		        // The person logged into your app
-		        FB.api('/me', function(response) {
-     			 	console.log('Good to see you, ' + response.name + '.');
-    			});
-		    } else {
-		        // The person cancelled the login dialog
-		    }
-		});
-	};
+      /*global FB */
+  		FB.login(function(response) {
+  			console.log(response.status);
+  		    if (response.authResponse) {
+  		        // The person logged into your app
+  		        FB.api('/me', function(response) {
+       			 	console.log('Good to see you, ' + response.name + '.');
+      			});
+  		    } else {
+  		        // The person cancelled the login dialog
+  		    }
+  		});
+	   };
 
   });
 
     // Additional JS functions here
-  window.fbAsyncInit = function() {
-    /*global FB */
-    FB.init({
-      appId      : '447182208718938', // App ID
-      channelUrl : '7fd7975b33e7c21185d476ae1141314c', // Channel File
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
-    });
-    
 
-};
 
 
 
 
 
   // Load the SDK asynchronously
-  (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
