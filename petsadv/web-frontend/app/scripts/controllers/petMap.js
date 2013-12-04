66eb33591ae3479aa33079a0a42c83a96c7e5957
ra2637 <ra2637@gmail.com>
2013-12-04 16:47:19 +0800

    add google map

--- petsadv/web-frontend/app/scripts/controllers/petMap.js
+++ petsadv/web-frontend/app/scripts/controllers/petMap.js
@@ -2,9 +2,17 @@
 
 angular.module('webFrontendApp')
   .controller('PetmapCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	var map;
+	function initialize() {
+	  var mapOptions = {
+	    zoom: 8,
+	    center: new google.maps.LatLng(23.5, 121)
+	  };
+	  map = new google.maps.Map(document.getElementsByClassName('map-canvas')[0],
+	      mapOptions);
+	};
+
+	$scope.loadMap = function(){
+		google.maps.event.addDomListener(window, 'load', initialize);
+	};
   });
