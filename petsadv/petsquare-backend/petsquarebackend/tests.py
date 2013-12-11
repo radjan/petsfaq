55d9c5635067aff47a58bb2858c1f04f404804e7
samuel <samuelololol@gmail.com>
2013-12-12 04:08:37 +0800

    add unittest example for location list api

--- petsadv/petsquare-backend/petsquarebackend/tests.py
+++ petsadv/petsquare-backend/petsquarebackend/tests.py
@@ -5,29 +5,48 @@ from pyramid import testing
 
 from .models import DBSession
 
+from routes import api_routes
+
 
 class TestMyView(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
         from sqlalchemy import create_engine
         engine = create_engine('sqlite://')
+
+        from .models.location import Location_TB
         from .models import (
             Base,
-            MyModel,
+            DBSession
             )
         DBSession.configure(bind=engine)
         Base.metadata.create_all(engine)
         with transaction.manager:
-            model = MyModel(name='one', value=55)
+            success, model = Location_TB.create(name='one', description='1', gps='1,5', address='taipei', userid=1)
             DBSession.add(model)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
+    def _callFUT(self, request):
+        from .apis.location import LocationAPI
+        import pyramid
+        api = LocationAPI(request=request, context=None)
+        return api.locations_list()
+
     def test_it(self):
-        from .views import my_view
+        #from .views import my_view
+        from .models.location import Location_TB
+        from .apis.location import LocationAPI
+        api_routes(self.config)
         request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'petsquare-backend')
+        request.matchdict = {'offset':1}
+        response = self._callFUT(request)
+
+        self.assertEqual(response['data'][0].name, 'one')
+        self.assertEqual(response['data'][0].description, '1')
+        self.assertEqual(response['data'][0].gps, '1,5')
+        self.assertEqual(response['data'][0].address, 'taipei')
+        self.assertEqual(response['data'][0].userid, 1)
+
