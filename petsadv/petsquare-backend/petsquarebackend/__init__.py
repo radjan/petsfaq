437a4f34aefdd3490884ac5a3ebcdbba74282dbd
samuel <samuelololol@gmail.com>
2013-12-27 04:37:18 +0800

    add fk option to sqlite

--- petsadv/petsquare-backend/petsquarebackend/__init__.py
+++ petsadv/petsquare-backend/petsquarebackend/__init__.py
@@ -1,5 +1,6 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
+from sqlalchemy import event
 from petsquarebackend.routes import api_routes
 
 from .models import (
@@ -7,11 +8,19 @@ from .models import (
     Base,
     )
 
+#add foreign key setting for sqlite, callback function
+def _fk_pragma_on_connect(dbapi_con, con_record):
+        dbapi_con.execute('pragma foreign_keys=ON')
+
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
+
+    #add foreign key setting for sqlite
+    event.listen(engine, 'connect', _fk_pragma_on_connect)
+
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
