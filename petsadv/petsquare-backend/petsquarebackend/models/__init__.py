390e95a52252f021cc053d9b70341112385f587b
samuel <samuelololol@gmail.com>
2014-01-03 04:52:48 +0800

    Revert "complete jsonify issue" not fixed This reverts commit a5995ce0d8ae10e4d492b03998ef81658aff85ba.

--- petsadv/petsquare-backend/petsquarebackend/models/__init__.py
+++ petsadv/petsquare-backend/petsquarebackend/models/__init__.py
@@ -227,25 +227,18 @@ class ModelMixin(object):
         return rtn
                                    
 
-    def __json__(self, request, pass_col=[], visited_tb=[]):
-        #log.debug('visit_tb: %s' % visited_tb)
+    def __json__(self, request, pass_col=[]):
         obj_dict = self.__dict__
         obj_dict = dict((key, obj_dict[key]) for key in obj_dict if not key.startswith("_"))
 
-        foreignkeys = list(self.__table__.foreign_keys)
-        foreignkeys = dict([(x.parent.name, x.column.table.name) for x in foreignkeys])
-        #foreignkeys = {fk_name: f_table, ...}
-
         rtn_dict = {}
         for k,value in obj_dict.items():
-            #log.debug('key name: %s, value: %s, value type: %s' % (k, value, type(value)))
+            log.debug('key name: %s, value: %s, value type: %s' % (k, value, type(value)))
             if k in pass_col:
                 continue
-            if all ([k in foreignkeys.keys(), foreignkeys.get(k, 'no foreignkeys mapping table') not in visited_tb]):
-                dfs_visited_tb = list(visited_tb)
-                dfs_visited_tb.append(foreignkeys[k])
-                value = self.get_by_id(value).__json__(request, visited_tb=dfs_visited_tb)
-                k = foreignkeys[k]
+            if k[-3:] == '_id':
+                value = self.__getattribute__(k[:-3])
+                log.debug(' transform key name: %s, value: %s, value type: %s' % (k[:-3], value, type(value)))
             if isinstance(value, datetime.datetime):
                 value = value.strftime("%Y-%m-%d, %H:%M:%S")
             rtn_dict[k] = value
