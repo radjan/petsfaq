1c34ca14f7dcbb8d76151bfac0cb07b71c6597c4
Rad Jan <jan.rad@gmail.com>
2014-01-23 22:14:51 +0800

    initial check inanimal service

---
+++ petsadv/petsquare-backend/petsquarebackend/services/animal.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__date__= 'Jan 23, 2014 '
+__author__= 'rad'
+
+import logging
+log = logging.getLogger(__name__)
+
+import inspect
+import traceback
+
+from petsquarebackend.services import BaseService
+from petsquarebackend.services import ServiceMethod
+from petsquarebackend.models.animal import Animal_TB
+
+class AnimalService(BaseService):
+    def __init__(self, request):
+        super(AnimalService, self).__init__('AnimalService', request)
+
+    @ServiceMethod
+    def list(self, finder_id, offset=0, size=100):
+        if finder_id:
+            success, models = Animal_TB.list(filattr=('finder_id', finder_id),
+                                      offset=offset,
+                                      size=size)
+        else:
+            success, models = Animal_TB.list(offset=offset,
+                                      size=size)
+
+        status = self.serv_rtn(success=success, model=models)
+        status['info']['count'] = len(models)
+        return status
+
+    @ServiceMethod
+    def create(cls, name, type, sub_type, status, description, finder_id,
+               find_location_id=None):
+        success, model = Animal_TB.create(name=name,
+                                          type=type,
+                                          sub_type=sub_type,
+                                          status=status,
+                                          description=description,
+                                          finder_id=finder_id,
+                                          find_location_id=find_location_id)
+        return self.serv_rtn(success=success, model=model)
+
+    @ServiceMethod
+    def show(self, id):
+        success, model = Animal_TB.show(id)
+        return self.serv_rtn(success=success, model=model)
+
+    @ServiceMethod
+    def update(self, id, data):
+        success, model = Animal_TB.update(id=id,
+                           name=data.get('name', None),
+                           type=data.get('type', None),
+                           sub_type=data.get('sub_type', None),
+                           description=data.get('description', None),
+                           finder_id=data.get('finder_id', None),
+                           find_location_id=data.get('find_location_id', None),)
+        return self.serv_rtn(success=success, model=model)
+
+    @ServiceMethod
+    def delete(self, id):
+        success, model = Animal_TB.delete(id=id)
+        return self.serv_rtn(success=success, model=model)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
