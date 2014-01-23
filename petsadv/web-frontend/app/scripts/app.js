4a926dc6bfbb00d54d03e57fcb0f87cb06c21847
ra2637 <ra2637@gmail.com>
2014-01-23 23:07:09 +0800

    add ezfb and complete fb login/out func

--- petsadv/web-frontend/app/scripts/app.js
+++ petsadv/web-frontend/app/scripts/app.js
@@ -6,9 +6,20 @@ angular.module('webFrontendApp', [
   'ngSanitize',
   'ngRoute',
   'ui.bootstrap',
-  'ui.map'
+  'ui.map',
+  'ezfb'
 ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $FBProvider) {
+    $FBProvider.setInitParams({
+      appId : '670257916371189',
+      channelUrl :'72e3cd86ab5e6fa4b680273374705beb',
+      // appId      : '365022226968758', // App ID
+      // channelUrl : 'efecb3e99c55926533debed6c33e63c5', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
--- petsadv/web-frontend/app/scripts/controllers/topNavBar.js
+++ petsadv/web-frontend/app/scripts/controllers/topNavBar.js
@@ -1,60 +1,55 @@
 'use strict';
 
 angular.module('webFrontendApp')
-  .controller('TopnavbarCtrl', function ($scope) {
+  .controller('TopnavbarCtrl', ['$scope', '$FB', function ($scope, $FB) {
     $scope.navBarList = [
       {name:"寵物地圖", url:"/#petMap"}
     ];
+    
     $scope.username="登入";
     $scope.login_check="Facebook 登入";
-    $scope.initFb = function(){
-      window.fbAsyncInit = function() {
-        /*global FB */
-        FB.init({
-          appId      : '365022226968758', // App ID
-          channelUrl : 'efecb3e99c55926533debed6c33e63c5', // Channel File
-          //appId      : '447182208718938', // App IDz
-          //channelUrl : '7fd7975b33e7c21185d476ae1141314c', // Channel Filez
-          status     : true, // check login status
-          cookie     : true, // enable cookies to allow the server to access the session
-          xfbml      : true  // parse XFBML
-        });
-      };
-
-      (function(d){
-         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-         if (d.getElementById(id)) {return;}
-         js = d.createElement('script'); js.id = id; js.async = true;
-         js.src = "//connect.facebook.net/en_US/all.js";
-         ref.parentNode.insertBefore(js, ref);
-       }(document));
+    updateLoginStatus(updateApiMe);
+    
+    $scope.logoutByFB = function () {
+      $FB.logout(function () {
+        updateLoginStatus(updateApiMe);
+      });
     };
 
     $scope.loginByFB=function(){
-      /*global FB */
-  		FB.login(function(response) {
-  			
-  		    if (response.authResponse) {
-  		        // The person logged into your app
-  		        FB.api('/me', function(response) {
-              $scope.username=response.name ;
-              console.log($scope.username);
-              $scope.login_check='Facebook登出';
-       			 	console.log('Good to see you, ' + response.name + '.');
-      			});
-  		    } else {
-  		        // The person cancelled the login dialog
-  		    }
-  		});
+      $FB.login(function (res) {
+        /**
+         * no manual $scope.$apply, I got that handled
+         */
+        if (res.authResponse) {
+          updateLoginStatus(updateApiMe);
+        }
+      }, {scope: 'email,user_likes'});
 	   };
 
-  });
 
-    // Additional JS functions here
+    function updateLoginStatus (more) {
+      $FB.getLoginStatus(function (res) {
+        $scope.loginStatus = res;
 
+        (more || angular.noop)();
+      });
+    }
 
+    function updateApiMe () {
+      if($scope.loginStatus.status === 'connected'){
+        $FB.api('/me', function (res) {
+          // $scope.apiMe = res;
+          $scope.username = res.username;
+          $scope.userPicture = "http://graph.facebook.com/"+res.id+"/picture";
+          $scope.login_check = "登出";
+        });  
+      }else{
+        $scope.username = "登入";
+        $scope.userPicture = "";
+        $scope.login_check = "Facebook 登入";
+      }
+      
+    }
 
-
-
-
-  // Load the SDK asynchronously
+  }]);
