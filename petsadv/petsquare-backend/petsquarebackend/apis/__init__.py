d1a1373d6ce67183fc6a6f2ebfab6bac50bc52f0
samuel <samuelololol@gmail.com>
2014-02-08 07:57:18 +0800

    1. add BaseAPP for mobile API, and route path 2. add locations list api for mobile-app, separated from original web api, 3. implement authenticated_userid() to search token from DB 4. use authenticated_userid() to transform the token to userid,    add to schema check replace the default value
    TODO:
    refactor whole api, separate the core function from api functions to helper
    functions, uses original apis function to wrap the latters, to support reusing
    by web api and mobile-app

--- petsadv/petsquare-backend/petsquarebackend/apis/__init__.py
+++ petsadv/petsquare-backend/petsquarebackend/apis/__init__.py
@@ -5,6 +5,7 @@ import logging
 log = logging.getLogger(__name__)
 
 from formencode import Invalid
+from pyramid.security import authenticated_userid
 
 class BaseAPI(object):
     def __init__(self, context, request):
@@ -39,6 +40,75 @@ class BaseAPI(object):
                     target_dict.update(dict(self.request.json_body.copy()))
             else:
                 target_dict.update(dict(self.request.params.copy()))
+
+            data = dict(target_dict)
+            data = schema.to_python(data)
+            rtn = (True, data, 200)
+
+        except Invalid, e:
+            errors = e.unpack_errors()
+            rtn = (False, errors, 400)
+        return rtn
+
+    def XHeaders(self, headers=['X-Requested-With'], origins=['*'],methods=[]):
+        header = ''
+        for h in headers:
+            header = header + ', ' + h
+        header = header[2:]
+        self.request.response.headers.add('Access-Control-Allow-Headers', header)
+
+        origin = ''
+        for o in origins:
+            origin = origin + ', ' + o
+        origin = origin[2:]
+        self.request.response.headers.add('Access-Control-Allow-Origin', origin)
+
+        method = ''
+        for m in methods:
+            method = method + ', ' + m
+        method = method[2:]
+        if len(methods) != 0:
+            self.request.response.headers.add('Access-Control-Allow-Methods', method)
+
+class BaseAPP(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def format_return(self, serv_rtn, fcode=500, scode=200):
+        """
+        Handle status returned from Services
+        """
+        status = serv_rtn
+        if status['success'] == True:
+            self.request.status_int = scode
+            rtn = {'data': status['data'], 'info': status['info']}
+        else:
+            self.request.status_int = fcode
+            rtn = {'data': status['data'], 'info': status['info']}
+        return rtn
+
+    def validate(self, schema, body=True):
+        """
+        Handle params sent from web requests
+        """
+        try:
+            #log.debug('request param: %s' % self.request.params)
+            #log.debug('request body: %s'  % self.request.body)
+            target_dict = dict()
+            to_check_dict = dict(self.request.params.copy())
+            token = to_check_dict.pop('token', None)
+            authn_userid = authenticated_userid(self.request)
+            #log.debug('authn_userid pop out???: %s' % authn_userid)
+            if authn_userid:
+                to_check_dict['userid'] = authn_userid[0]
+
+            if body == True:
+                target_dict.update(to_check_dict)
+                if len(self.request.body) > 0:
+                    target_dict.update(dict(self.request.json_body.copy()))
+            else:
+                target_dict.update(to_check_dict)
 
             data = dict(target_dict)
             data = schema.to_python(data)
--- petsadv/petsquare-backend/petsquarebackend/authentication.py
+++ petsadv/petsquare-backend/petsquarebackend/authentication.py
@@ -3,6 +3,9 @@
 __date__= 'Feb 07, 2014 '
 __author__= 'samuel'
 
+import logging
+log = logging.getLogger(__name__)
+
 from zope.interface import implementer
 
 from pyramid.interfaces import IAuthenticationPolicy
@@ -12,13 +15,22 @@ from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.httpexceptions import HTTPUnauthorized
 from pyramid.util import DottedNameResolver
 
+from petsquarebackend.services.accounts import AccountService
 
 @implementer(IAuthenticationPolicy)
 class TokenAuthenticationPolicy(CallbackAuthenticationPolicy):
     def unauthenticated_userid(self, request):
-        return 1
-    def unauthenticated_userid(self, request):
-        return 1
+        return []
+    def authenticated_userid(self, request):
+        token = request.params.get('token', None)
+        acc_service = AccountService(request)
+        serv_rtn = acc_service.sso_check(token)
+        rtn_list = []
+        if serv_rtn['data']:
+            rtn_list = [serv_rtn['data'].id]
+        else:
+            rtn_list = []
+        return rtn_list
     def effective_principals(self, request):
         return [Everyone]
     def remember(self, request, principal, **wk):
