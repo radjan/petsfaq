340908b3ed182a6c2c938b29f4f08ecca46a8dcb
samuelololol <samuelololol@gmail.com>
2014-02-08 21:20:54 +0800

    change exception log level

--- petsadv/petsquare-backend/petsquarebackend/models/__init__.py
+++ petsadv/petsquare-backend/petsquarebackend/models/__init__.py
@@ -244,7 +244,7 @@ class ModelMixin(object):
     def model_exception_rtn(cls, exp, ins_stk, tbk):
         rtn = []
         err_info = (cls.__tablename__, ins_stk, tbk)
-        log.debug('%s:%s, traceback:\n %s' % err_info)
+        log.error('%s:%s, traceback:\n %s' % err_info)
         rtn.append(False)
         rtn.append({'status':'fail', 'msg':'model error on %s' % ins_stk})
         return rtn
