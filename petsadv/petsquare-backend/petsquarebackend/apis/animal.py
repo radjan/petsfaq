31b4932ce61bf2f43127404dc802dd8fa1d3f069
Rad Jan <jan.rad@gmail.com>
2014-02-22 16:13:29 +0800

    animal validation

--- petsadv/petsquare-backend/petsquarebackend/apis/animal.py
+++ petsadv/petsquare-backend/petsquarebackend/apis/animal.py
@@ -17,31 +17,64 @@ from pyramid.view import (
         view_defaults,
         )
 
+from petsquarebackend import apis
 from petsquarebackend.apis import BaseAPI
 from petsquarebackend.apis import BaseAPP
 from petsquarebackend.services.animal import AnimalService
 
+RESERVED = apis.RESERVED
+IGNORE = apis.IGNORE
+DATETIME_FORMAT = apis.DATETIME_FORMAT
 
-class Schema_animals_get(Schema):
+PARAMS = ('id', 'name', 'type', 'sub_type', 'status', 'description', 
+          'finder_id', 'owner_id', 'find_location', 'current_location')
+
+class SchemaAnimalsGet(Schema):
     offset = validators.Int(if_missing=0)
     size   = validators.Int(if_missing=100)
-    finder_id =  validators.Int(if_missing=1)
 
-class Schema_animal_post(Schema):
-    name        = validators.UnicodeString(if_missing=u'AnimalName')
-    type        = validators.UnicodeString(if_missing=u'type')
-    sub_type    = validators.UnicodeString(if_missing=u'sub_type')
-    status      = validators.UnicodeString(if_missing=u'help')
-    description = validators.UnicodeString(if_missing=u'')
-    finder_id   = validators.Int(if_missing=1)
+    id          = validators.Int(if_missing=IGNORE)
+    name        = validators.UnicodeString(if_missing=IGNORE)
+    type        = validators.UnicodeString(if_missing=IGNORE)
+    sub_type    = validators.UnicodeString(if_missing=IGNORE)
+    status      = validators.UnicodeString(if_missing=IGNORE)
+    finder_id   = validators.Int(if_missing=IGNORE)
+    owner_id    = validators.Int(if_missing=IGNORE)
+    find_location_id    = validators.Int(if_missing=IGNORE)
+    current_location_id = validators.Int(if_missing=IGNORE)
 
-class Schema_animal_put(Schema):
+class SchemaAnimalsPost(Schema):
+    # required
     name        = validators.UnicodeString()
     type        = validators.UnicodeString()
     sub_type    = validators.UnicodeString()
     status      = validators.UnicodeString()
-    description = validators.UnicodeString()
-    finder_id   = validators.Int()
+
+    # optional
+    description = validators.UnicodeString(if_missing=IGNORE)
+    owner_id    = validators.Int(if_missing=IGNORE)
+    find_location_id    = validators.Int(if_missing=IGNORE)
+    current_location_id = validators.Int(if_missing=IGNORE)
+
+    # overwrited in create, valid in update
+    finder_id   = validators.Int(if_missing=IGNORE)
+
+class SchemaAnimalPut(SchemaAnimalsPost):
+    # optinal: overwrite
+    name        = validators.UnicodeString(if_missing=IGNORE)
+    type        = validators.UnicodeString(if_missing=IGNORE)
+    sub_type    = validators.UnicodeString(if_missing=IGNORE)
+    status      = validators.UnicodeString(if_missing=IGNORE)
+
+    # allow these attributes, not interested anyway
+    id          = validators.Int(if_missing=IGNORE)
+    finder      = validators.UnicodeString(if_missing=IGNORE)
+    owner       = validators.UnicodeString(if_missing=IGNORE)
+    find_location   = validators.UnicodeString(if_missing=IGNORE)
+    current_location= validators.UnicodeString(if_missing=IGNORE)
+    createddatetime = validators.UnicodeString(if_missing=IGNORE)
+    updateddatetime = validators.UnicodeString(if_missing=IGNORE)
+    accepter_assocs = validators.UnicodeString(if_missing=IGNORE)
 
 class BaseAnimal(object):
     """
@@ -53,19 +86,17 @@ class BaseAnimal(object):
         API: GET /animals
         """
         #validation
-        success, data, code = self.validate(Schema_animals_get)
+        success, data, code = self.validate(SchemaAnimalsGet)
 
         if success:
             serv = AnimalService(self.request)
-            serv_rtn = serv.list(finder_id=data['finder_id'], 
+            params = dict((k, v) for k, v in data.items()
+                                    if k in PARAMS and v is not IGNORE)
+            serv_rtn = serv.list(params=params,
                                  offset=data['offset'],
                                  size=data['size'])
         else:
-            #mock fake serv_rtn
-            serv_rtn = {'data':'', 
-                        'info':data, 
-                        'code':code, 
-                        'success':False}
+            serv_rtn = self._validation_error(data, code)
 
         api_rtn = self.format_return(serv_rtn)
         return api_rtn
@@ -76,7 +107,7 @@ class BaseAnimal(object):
         API: GET /animal/<id:\d+>
         """
         #validation
-        success, data, code = self.validate(Schema_animals_get)
+        success, data, code = self.validate(SchemaAnimalsGet)
 
         try:
             #get id from route_path
@@ -88,12 +119,7 @@ class BaseAnimal(object):
             serv = AnimalService(self.request)
             serv_rtn = serv.show(id=animalid)
         else:
-            #mock fake serv_rtn
-            serv_rtn = {'data':'',
-                        'info':data,
-                        'code':code,
-                        'success':False,
-                        }
+            serv_rtn = self._validation_error(data, code)
 
         api_rtn = self.format_return(serv_rtn)
         return api_rtn
@@ -104,18 +130,16 @@ class BaseAnimal(object):
         API: POST /animals
         """
         #validation
-        success, data, code = self.validate(Schema_animals_post)
+        success, data, code = self.validate(SchemaAnimalsPost)
 
         if success:
             serv = AnimalService(self.request)
-            data['finder_id'] = data.pop['user_id']
-            serv_rtn = serv.create(data)
+            params = dict((k, v) for k, v in data.items()
+                                    if k in PARAMS and v is not IGNORE)
+            params['finder_id'] = data['user_id']
+            serv_rtn = serv.create(params)
         else:
-            #mock fake serv_rtn
-            serv_rtn = {'data':'', 
-                        'info':data, 
-                        'code':code, 
-                        'success':False}
+            serv_rtn = self._validation_error(data, code)
 
         api_rtn = self.format_return(serv_rtn)
         return api_rtn
@@ -126,7 +150,7 @@ class BaseAnimal(object):
         API: PUT /animal/<id:\d+>
         """
         #validation
-        success, data, code = self.validate(Schema_animal_put)
+        success, data, code = self.validate(SchemaAnimalPut)
 
         try:
             #get id from route_path
@@ -136,14 +160,11 @@ class BaseAnimal(object):
 
         if success:
             serv = AnimalService(self.request)
-            serv_rtn = serv.update(id=animalid, data=data)
+            params = dict((k, v) for k, v in data.items()
+                                    if k in PARAMS and v is not IGNORE)
+            serv_rtn = serv.update(id=animalid, data=params)
         else:
-            #mock fake serv_rtn
-            serv_rtn = {'data':'',
-                        'info':data,
-                        'code':code,
-                        'success':False,
-                        }
+            serv_rtn = self._validation_error(data, code)
 
         api_rtn = self.format_return(serv_rtn)
         return api_rtn
@@ -154,7 +175,7 @@ class BaseAnimal(object):
         API: DELETE /animal/<id:\d+>
         """
         #validation
-        success, data, code = self.validate(Schema_animals_get)
+        success, data, code = self.validate(SchemaAnimalsGet)
 
         try:
             #get id from route_path
@@ -166,12 +187,7 @@ class BaseAnimal(object):
             serv = AnimalService(self.request)
             serv_rtn = serv.delete(id=animalid)
         else:
-            #mock fake serv_rtn
-            serv_rtn = {'data':'',
-                        'info':data,
-                        'code':code,
-                        'success':False,
-                        }
+            serv_rtn = self._validation_error(data, code)
 
         api_rtn = self.format_return(serv_rtn)
         return api_rtn
